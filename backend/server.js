6076a36d53901fa8ca747e504c12c21253b9917f
Naim Ahmad <naimahmad201103@gmail.com>
2023-09-13 17:01:51 +0600

    created server

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const chats = require('./data/data')
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/api/chats', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find(data=> data._id === req.params.id)
+    res.send(singleChat)
+})
+
+app.listen(PORT, console.log('Server is running on PORT ' + PORT))
